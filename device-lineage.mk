ed12da34bad976dfd40306100c8b81fb3fa00e18
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:28 +0530

    redfin: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Calyx Institute
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,6 +12,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
 
 # CHRE
 $(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/redfin:libadsprpc)
+
+# Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/redfin/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
 # HBM
 PRODUCT_PACKAGES += \
