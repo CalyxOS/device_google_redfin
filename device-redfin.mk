d9f73324fcc1bb811c4366dc87cf00050db542b7
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-30 21:53:22 +0530

    Compile with prebuilt kernel conditionally
    Change-Id: I1ec046d186897f70809722c18c1e8b3644da5ed7

--- device-redfin.mk
+++ device-redfin.mk
@@ -16,17 +16,14 @@
 
 PRODUCT_HARDWARE := redfin
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-        LOCAL_KERNEL := device/google/redfin-kernel/Image.lz4
-    else
-        LOCAL_KERNEL := device/google/redfin-kernel/vintf/Image.lz4
-    endif
+ifeq ($(INLINE_KERNEL_BUILDING),false)
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+    TARGET_PREBUILT_KERNEL := device/google/redfin-kernel/Image.lz4
 else
-    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+    TARGET_PREBUILT_KERNEL := device/google/redfin-kernel/vintf/Image.lz4
 endif
-
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/redfin-kernel/sm7250/kernel-headers
+endif
 
 include build/make/target/product/iorap_large_memory_config.mk
 include device/google/redbull/device-common.mk
