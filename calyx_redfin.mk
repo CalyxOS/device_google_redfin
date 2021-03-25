0fde12abc186c0fafdfc28c34778cb1b67f72243
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: Id842f75cc0d3888832965e3d338b9611f8e9e23e

--- calyx_redfin.mk
+++ calyx_redfin.mk
@@ -8,6 +8,8 @@ DEVICE_PACKAGE_OVERLAYS += device/google/redfin/overlay-calyx
 $(call inherit-product, device/google/redbull/device-calyx.mk)
 $(call inherit-product, device/google/redfin/aosp_redfin.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/redfin/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_redfin
 PRODUCT_MODEL := Pixel 5
