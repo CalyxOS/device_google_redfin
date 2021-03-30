57031facf0d53a025fc971e7dbc116fa2090320c
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    redfin: Implement PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The Calyx Institute
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,3 +18,7 @@
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/redfin/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.redfin
--- redfin/BoardConfig-calyx.mk
+++ redfin/BoardConfig-calyx.mk
@@ -6,4 +6,10 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
+# Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/redfin/lineage_compatibility_matrix.xml
+
+# SEPolicy
+BOARD_SEPOLICY_DIRS += device/google/redfin/sepolicy-lineage/vendor
+
 include vendor/google/redfin/BoardConfigVendor.mk
