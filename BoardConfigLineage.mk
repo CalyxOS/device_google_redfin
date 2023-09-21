d5283a1b128a2301fd35faf5e4293e94d6cad5d7
Michael Bestas <mkbestas@gmail.com>
2023-09-21 19:28:44 +0300

    redfin: Set platform security patch level
    Change-Id: I0d7c997221eab361c4566f73562bab2336085334

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -10,6 +10,7 @@ BOOT_KERNEL_MODULES += sec_touch.ko
 include device/google/redbull/BoardConfigLineage.mk
 
 BOOT_SECURITY_PATCH := 2023-11-05
+PLATFORM_SECURITY_PATCH := 2023-11-05
 VENDOR_SECURITY_PATCH := 2023-11-05
 
 include vendor/google/redfin/BoardConfigVendor.mk
