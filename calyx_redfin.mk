18c4b9a429533df24c7292a4daf445013123f22a
Michael Bestas <mkbestas@gmail.com>
2024-10-17 18:57:23 +0300

    Switch to new property override scheme
    Change-Id: I6baaec99e78f79f6b84c737a613f7e61048ba895

--- calyx_redfin.mk
+++ calyx_redfin.mk
@@ -14,11 +14,10 @@ PRODUCT_NAME := calyx_redfin
 PRODUCT_MODEL := Pixel 5
 PRODUCT_BRAND := google
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 14 UP1A.231105.001.B2 11260668 release-keys"
-
-BUILD_FINGERPRINT := google/redfin/redfin:14/UP1A.231105.001.B2/11260668:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES := \
+    BuildDesc=redfin-user[[:space:]]15[[:space:]]UP1A.231105.001.B2[[:space:]]11260668[[:space:]]release-keys \
+    BuildFingerprint=google/redfin/redfin:15/UP1A.231105.001.B2/11260668:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
